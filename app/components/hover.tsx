77f60e34c55df3012aff28be7dd481716b7e4b5e
Naman Sharma <namansharmans03@gmail.com>
2026-03-09 22:58:25 +0530

    3d effect button BASIC

---
+++ app/components/hover.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hover = () => {
+  return (
+    <div className=''>
+  
+      <button className="px-4 py-2 bg-green-500 text-white font-semibold rounded-lg shadow-[0_4px_0_rgb(21,128,61)] hover:bg-green-600 hover:shadow-[0_4px_0_rgb(22,101,52)] active:translate-y-1 active:shadow-[0_0px_0_rgb(22,101,52)] transition-all duration-100">
+  3D Press Button
+</button>
+    </div>
+  )
+}
+
+export default Hover
+
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ import { Grid } from "./components/grid";
 import { ModeToggle } from "./components/mode-toggle";
 import SvgCompoent from "./components/svg-compoent";
 import FileSvg from "./components/file_Svg";
-
+import Hover from "./components/hover"
 export default function Home() {
   return (
     <div className="h-screen max-w-4xl mx-auto flex flex-col items-center justify-center gap-10 bg-neutral-100">
-<FileSvg />
+<Hover/>
 
     </div>
   );
